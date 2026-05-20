691514994f6f4c907624ea7e7fbdf8044ea90614
RocioCopa <63129056+Rocio-20@users.noreply.github.com>
2026-05-20 12:30:51 -0300

    Diagrama de clases

---
+++ src/main/java/ar.edu.utn.frba.dds.donatrack/clasificacion/Categoria.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.frba.dds.donatrack.clasificacion;
+
+public class Categoria {
+}
---
+++ src/main/java/ar.edu.utn.frba.dds.donatrack/donante/PersonaJuridica.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.frba.dds.donatrack.donante;
+
+public class PersonaJuridica {
+}
