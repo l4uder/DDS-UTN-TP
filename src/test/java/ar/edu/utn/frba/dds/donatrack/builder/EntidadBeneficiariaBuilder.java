dd40ba5de0b89f54ddfd3cce4cda312b8a148c8e
RocioCopa <63129056+Rocio-20@users.noreply.github.com>
2026-06-20 22:14:55 -0300

    Test entregas y rutas

--- src/test/java/ar/edu/utn/frba/dds/donatrack/builder/EntidadBeneficiariaBuilder.java
+++ src/test/java/ar/edu/utn/frba/dds/donatrack/builder/EntidadBeneficiariaBuilder.java
@@ -1,4 +1,39 @@
 package ar.edu.utn.frba.dds.donatrack.builder;
 
+import ar.edu.utn.frba.dds.donatrack.EntregaTest;
+import ar.edu.utn.frba.dds.donatrack.dominio.beneficiario.EntidadBeneficiaria;
+import ar.edu.utn.frba.dds.donatrack.dominio.mediocontacto.MedioContacto;
+import ar.edu.utn.frba.dds.donatrack.dominio.necesidades.Necesidad;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
 public class EntidadBeneficiariaBuilder {
+  private String razonSocial;
+  private String direccion;
+  private List<MedioContacto> contactoRepresentantes;
+  private List<Necesidad> necesidades;
+
+  public EntidadBeneficiariaBuilder(){
+    this.necesidades = new ArrayList<>();
+  }
+
+  public EntidadBeneficiariaBuilder conRazonSocial(String razonSocial) {
+    this.razonSocial = razonSocial;
+    return this;
+  }
+
+  public EntidadBeneficiariaBuilder conDireccion(String direccion) {
+    this.direccion = direccion;
+    return this;
+  }
+
+  public EntidadBeneficiariaBuilder conMediosContactos(List<MedioContacto> medioContactos) {
+    this.contactoRepresentantes = medioContactos;
+    return this;
+  }
+
+  public EntidadBeneficiaria build(){
+    return new EntidadBeneficiaria(razonSocial, direccion, contactoRepresentantes);
+  }
 }
