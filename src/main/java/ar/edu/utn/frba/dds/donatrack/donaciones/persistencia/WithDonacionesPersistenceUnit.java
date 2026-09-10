d658310099e75e02b19a2d9a6aaa4b2f8e6e00d6
RocioCopa <63129056+Rocio-20@users.noreply.github.com>
2026-09-09 23:27:24 -0300

    fix: persistencia separada por microservicio

--- src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/persistencia/WithDonacionesPersistenceUnit.java
+++ src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/persistencia/WithDonacionesPersistenceUnit.java
@@ -1,4 +1,19 @@
 package ar.edu.utn.frba.dds.donatrack.donaciones.persistencia;
 
-public interface WhitDonacionesPersisntenceUnit {
+import io.github.flbulgarelli.jpa.extras.EntityManagerOps;
+import io.github.flbulgarelli.jpa.extras.TransactionalOps;
+import io.github.flbulgarelli.jpa.extras.perthread.PerThreadEntityManagerAccess;
+import io.github.flbulgarelli.jpa.extras.perthread.WithPerThreadEntityManager;
+
+public interface WithDonacionesPersistenceUnit
+    extends WithPerThreadEntityManager, EntityManagerOps, TransactionalOps {
+
+  String DONACIONES_PERSISTENCE_UNIT_NAME = "donaciones-persistence-unit";
+  PerThreadEntityManagerAccess PER_THREAD_ENTITY_MANAGER_ACCESS =
+      new PerThreadEntityManagerAccess(DONACIONES_PERSISTENCE_UNIT_NAME);
+
+  @Override
+  default PerThreadEntityManagerAccess perThreadEntityManagerAccess() {
+    return PER_THREAD_ENTITY_MANAGER_ACCESS;
+  }
 }
--- src/main/java/ar/edu/utn/frba/dds/donatrack/logistica/persistencia/WithLogisticaPersistenceUnit.java
+++ src/main/java/ar/edu/utn/frba/dds/donatrack/logistica/persistencia/WithLogisticaPersistenceUnit.java
@@ -1,4 +1,18 @@
 package ar.edu.utn.frba.dds.donatrack.logistica.persistencia;
 
-public class WhitLogisticaPersistenceUnit {
+import io.github.flbulgarelli.jpa.extras.EntityManagerOps;
+import io.github.flbulgarelli.jpa.extras.TransactionalOps;
+import io.github.flbulgarelli.jpa.extras.perthread.PerThreadEntityManagerAccess;
+import io.github.flbulgarelli.jpa.extras.perthread.WithPerThreadEntityManager;
+
+public interface WithLogisticaPersistenceUnit extends WithPerThreadEntityManager, EntityManagerOps, TransactionalOps {
+
+  String LOGISTICA_PERSISTENCE_UNIT_NAME = "logistica-persistence-unit";
+  PerThreadEntityManagerAccess PER_THREAD_ENTITY_MANAGER_ACCESS =
+      new PerThreadEntityManagerAccess(LOGISTICA_PERSISTENCE_UNIT_NAME);
+
+  @Override
+  default PerThreadEntityManagerAccess perThreadEntityManagerAccess() {
+    return PER_THREAD_ENTITY_MANAGER_ACCESS;
+  }
 }
