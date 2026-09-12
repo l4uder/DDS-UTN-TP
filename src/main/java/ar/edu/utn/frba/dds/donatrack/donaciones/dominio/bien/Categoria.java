d4ce17457feca7eeda76f98874e90f80133b4762
Joaquin Montagnese <joaquin.montagnese@gmail.com>
2026-09-12 19:44:06 -0300

    sumando categoria

--- src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/dominio/bien/Categoria.java
+++ src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/dominio/bien/Categoria.java
@@ -1,9 +1,21 @@
 package ar.edu.utn.frba.dds.donatrack.donaciones.dominio.bien;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@Entity
+@Table(name="Categoria")
 public class Categoria {
+  @Id @GeneratedValue()
+  @Column(name="id_categoria")
+  private Long id;
+  @Column(name="nombre")
   private String nombre;
 
   public Categoria(String nombre) {
