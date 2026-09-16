9f77dc759f6b8a3f54dba98490d64c93be3b6ae2
Joaquin Montagnese <joaquin.montagnese@gmail.com>
2026-09-16 18:00:37 -0300

    creando subcategoria tabla

--- src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/dominio/bien/Subcategoria.java
+++ src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/dominio/bien/Subcategoria.java
@@ -1,10 +1,25 @@
 package ar.edu.utn.frba.dds.donatrack.donaciones.dominio.bien;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Getter;
-
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@Entity
+@Table(name="Subcategoria")
 @Getter
 public class Subcategoria {
+  @Id @GeneratedValue
+  @Column (name="id_subcategoria")
+  private Long id;
+  @Column (name = "nombre")
   private String nombre;
+  @ManyToOne
+  @JoinColumn(name = "id_categoria")
   private Categoria categoria;
 
   public Subcategoria(String nombre,
