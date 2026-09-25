ff3112f35aa1b80e5b2bf704988261a8d8bf1335
david <davidmesber@hotmail.com>
2026-09-25 13:50:21 -0300

    Agrego persistencia de Donacion en Ranking

--- microservicio-donacion/src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/dominio/generadorrankings/Ranking.java
+++ microservicio-donacion/src/main/java/ar/edu/utn/frba/dds/donatrack/donaciones/dominio/generadorrankings/Ranking.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -28,7 +29,8 @@ public class Ranking {
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id_ranking")
   private Long id;
-  @Transient
+  @ManyToOne
+  @JoinColumn(name = "id_donacion")
   private Donacion donacion;
   @ManyToMany
   @JoinTable (
